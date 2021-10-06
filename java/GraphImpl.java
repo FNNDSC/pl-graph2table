667e499e159f8727cfc3a4f978ba80985fc3dfe7
Sandip Samal <sandip117@github.com>
2021-10-05 20:39:52 -0400

    Modified impl to create graph

--- java/GraphImpl.java
+++ java/GraphImpl.java
@@ -2,33 +2,55 @@ import java.util.List;
 import java.util.ArrayList;
 public class GraphImpl {
 
+    private Graph graph;
+
+    public Graph getGraph(){
+
+	return this.graph;
+	
+    }
+    GraphImpl(int[][]adjMat){
+
+	int n = adjMat.length;
+
+	// Lets create the vertices
+	List<Vertex> vertices = new ArrayList<>();
+	for(int v=0; v<n;v++){
+	    Vertex ver = new Vertex(Integer.toString(v));
+	    vertices.add(ver);
+	}
+
+	
+	// Lets create edges
+	List<Edge> edges = new ArrayList<>();
+	for(int i =0;i<n; i++ ){
+	    for(int j=0; j<n; j++){
+		if(adjMat[i][j]==1){
+                    // if path exists, create an edge
+		    Edge eg = new Edge(vertices.get(i),vertices.get(j),0,0,0);
+		    edges.add(eg);
+		    
+		}
+	    }
+	}
+
+	this.graph = new Graph(vertices,edges); 
+	    
+    }
+
     public static void main (String args[]){
 
-        Vertex v1 = new Vertex("a");
-	Vertex v2 = new Vertex("b");
-	Vertex v3 = new Vertex("c");
-	Vertex v4 = new Vertex("d");
+	int [][] arr = {{1,0,1,0},{0,0,0,0},{1,1,1,1},{0,1,0,1}};
 
-	Edge e1 = new Edge(v1,v2,10,10,10);
-	Edge e2 = new Edge(v2,v3,10,10,10);
-	Edge e3 = new Edge(v3,v4,10,10,10);
 
-	List<Vertex> vertices = new ArrayList <>();
-	vertices.add(v1);
-	vertices.add(v2);
-	vertices.add(v3);
-	vertices.add(v4);
+	GraphImpl grphImpl = new GraphImpl(arr);
+       
 
-	List<Edge> edges = new ArrayList<>();
-	edges.add(e1);
-	edges.add(e2);
-	edges.add(e3);
-
-	Graph g1 = new Graph (vertices,edges);
+	Graph g1 = grphImpl.getGraph();
 
 	GraphSearch gsImpl = new GraphSearch(g1);
 
-	gsImpl.BFS(v1);
+	gsImpl.BFS(g1.getVertices().get(0));
 
 	for(Vertex v :g1.getVertices()){
 	    System.out.println(v.name + ":" + (v.getParent()!=null? v.getParent().name:"Root"));
